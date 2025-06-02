03db9d53f5d1ab3bad4422b25af4a626f92e842b
hkim8877 <hkim8877@gmail.com>
2025-06-02 18:19:08 +0300

    gnl

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,4 +1,9 @@
 char *get_next_line(int fd)
 {
-	
+	static char *lines = NULL:
+	char *next_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 }
+char *
--- gnl/get_next_line_utils.c
+++ gnl/get_next_line_utils.c
@@ -3,6 +3,8 @@ int ft_strlen(const char *str)
 	int len;
 
 	len = 0;
+	if (!str)
+		return (0);
 	while (str[len])
 		len++;
 	return (len);
@@ -10,19 +12,41 @@ int ft_strlen(const char *str)
 
 char *ft_strchr(const char *str, int c)
 {
+	if (!str)
+		return (NULL);
 	while (*str)
 	{
 		if (*str == (char)c)
 			return ((char *)str);
 		str++;
 	}
-	if (*str == t)
+	if (c == '\0')
 		return ((char *)str);
 	return (NULL);
 }
 char *ft_strdup(const char *src)
 {
+	int len;
+	int i;
+	char *dest;
 
+	if (!src)
+		return (NULL);
+	len = ft_strlen(src);
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
-char *ft_strjoin(const char *str1, const char *str2)
+char *ft_strjoin(const char *s1, const char *s2)
+{
+	
+}
