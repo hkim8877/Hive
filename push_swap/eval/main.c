3d21cac697e4626cf4163c5dfb28e85df6699383
hkim8877 <hkim8877@gmail.com>
2025-07-11 14:00:47 +0300

    7.11

---
+++ push_swap/eval/main.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunjkim <hyunjkim@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/07 18:04:53 by hyunjkim          #+#    #+#             */
+/*   Updated: 2025/07/07 18:04:56 by hyunjkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static char	**parse_argv(int argc, char **argv, int *split)
+{
+	if (argc == 2)
+	{
+		*split = 1;
+		if (!*argv[1])
+			error();
+		return (ft_split(argv[1], ' '));
+	}
+	return (argv);
+}
+
+static void	free_split(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
+
+#include <stdio.h>
+
+static int	init_stack_from_args(t_stack **a, char **args, int split)
+{
+	int		start;
+	long	value;
+	int		i;
+
+	if (split)
+		start = 0;
+	else
+		start = 1;
+	i = start;
+	while (args[i])
+	{
+		if (check_errors(args[i]))
+		{
+			free_stack(a);
+			if (split)
+				free_split(args);
+			return (0);
+		}
+		value = ft_atol(args[i]);
+		stack_init(a, value);
+		i++;
+	}
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
+	char	**splited;
+	int		split;
+
+	a = NULL;
+	b = NULL;
+	split = 0;
+	if (argc < 2)
+		return (1);
+	splited = parse_argv(argc, argv, &split);
+	if (!splited)
+		return (1);
+	if (!init_stack_from_args(&a, splited, split))
+		return (cleanup_and_error(&a, &b));
+	if (check_duplicate(a))
+		return (cleanup_and_error(&a, &b));
+	if (!is_sorted(&a))
+		push_swap(&a, &b);
+	free_stack(&a);
+	free_stack(&b);
+	if (split)
+		free_split(splited);
+	return (0);
+}
