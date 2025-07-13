65f0e14b5ff5f48612e97df4621c2a4658b7f060
hkim8877 <hkim8877@gmail.com>
2025-07-13 22:31:29 +0300

    7.13

--- pipex/main.c
+++ pipex/main.c
@@ -1,6 +1,28 @@
 #include "pipex.h"
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **envp)
 {
 
 }
+/*
+int pipe(int fd[2]);
+
+int access(const char *path, int mode);
+
+pid_t wait(int *stat_loc);
+
+pid_t waitpid(pid_t pid, int *stat_loc, int options);
+
+int dup(int fd);
+
+int dup2(int fd, int fd2);
+
+pid_t fork(void);
+
+int execve(const char *filename, char *const argv[], char *const envp[]);
+
+void perror(cosnt char *str);
+
+char *strerror(int errnum);
+
+*/
